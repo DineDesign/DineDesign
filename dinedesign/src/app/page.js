ec4b16000e37ee10918572ad7ab304335b72c76a
Vicheka Oeun <oeunvicheka95@gmail.com>
2024-07-14 16:53:55 -0400

    add title card

--- dinedesign/src/app/page.js
+++ dinedesign/src/app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Footer from "./components/Footer"
 import InfiniteScroll from "./components/InfiniteScroll";
+import Title from "./components/Title";
 export default function Home() {
   return (
-  <>
-  <Footer/>
-  {/* <InfiniteScroll/> */}
-  
-  </>
+    <>
+      <Title />
+      <Footer />
+      {/* <InfiniteScroll/> */}
+
+    </>
   );
 }
