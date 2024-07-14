c8176c8a5697742dc9950d6b7e2d49cb66a1eb22
Dave <davinder2007@gmail.com>
2024-07-14 13:55:00 -0400

    Add footer code, add Infite scroll (doesnt work atm)

--- dinedesign/src/app/page.js
+++ dinedesign/src/app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Footer from "./components/Footer"
-
+import InfiniteScroll from "./components/InfiniteScroll";
 export default function Home() {
   return (
   <>
   <Footer/>
+  {/* <InfiniteScroll/> */}
   
   </>
   );
